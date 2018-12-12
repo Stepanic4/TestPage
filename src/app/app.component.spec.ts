4868e7d88def2d2821629c747a948804b3d2c41d
Ivan <Zolotukhin.ivan.1@gmail.com>
2018-12-12 14:27:47 +0200

    clear old project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'WodProof'`, () => {
+  it(`should have as title 'TestPage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('WodProof');
+    expect(app.title).toEqual('TestPage');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to WodProof!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TestPage!');
   });
 });
